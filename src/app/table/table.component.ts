ea900484765ea3714a6461b6dce840538b4d5d3a
Batman-DevOps <120541615+Batman-DevOps@users.noreply.github.com>
2023-02-28 09:26:40 +0530

    2 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,20 +1,55 @@
 import { Component, Input } from '@angular/core';
+import { FormArray } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'my-table',
+  selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: [ './table.component.scss' ]
+  styleUrls: ['./table.component.scss'],
 })
-export class TableComponent  {
-  
-  @Input() address: FormGroup;
-  
-  constructor(private fb: FormBuilder) {}
-  
-  ngOnInit() {
-    this.address = fb.group({
+export class TableComponent {
+  @Input() address: FormArray;
+  addressForm: FormGroup;
 
-    })
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.addressForm = this.fb.group({
+      addresses: this.address
+    });
+    // this.address = this.fb.array([]);
+
+    const addressArray = [
+      {
+        street: 'MG Road',
+        city: 'Goa',
+        state: 'Maharashtra',
+      },
+      {
+        street: 'MG Road',
+        city: 'Goa',
+        state: 'Maharashtra',
+      },
+      {
+        street: 'MG Road',
+        city: 'Goa',
+        state: 'Maharashtra',
+      },
+    ];
+
+    addressArray.forEach((ele) => {
+      this.addAddress(ele);
+    });
+  }
+
+  addAddress(address: any) {
+    let addressFormGroup = this.fb.group({
+      street: address.street,
+      city: address.city,
+      state: address.state,
+    });
+
+    this.addressForm.value.addresses.push(addressFormGroup);
+    console.log('this.addressForm.value', this.addressForm.value);
   }
 }
